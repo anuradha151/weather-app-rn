a7dfde52ad8a77d2c7c978c93787659201245847
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-04-02 07:13:22 +0530

    Feature : add navigation container

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpComingWeather from './src/screens/UpComingWeather';
 import City from './src/screens/City';
-
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <City />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <City />
+      </View>
+    </NavigationContainer>
   );
 }
 
